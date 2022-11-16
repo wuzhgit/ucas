045c39b758c5604c97954a779cb8f1a3cfa7de14
wuzhgit <107784355+wuzhgit@users.noreply.github.com>
2022-11-16 16:21:43 +0800

    sub.py

--- sub.py
+++ sub.py
@@ -96,50 +96,64 @@ def submit(s: requests.Session, old: dict):
     new_daily = {
         'realname': old['realname'],
         'number': old['number'],
-        'szgj_api_info': old['szgj_api_info'],
-        # 'szgj': old['szgj'],# 2021.8.1 del
-        # 'old_sfzx': old['sfzx'],# 2021.8.1 del
-        'sfzx': old['sfzx'],
-        'szdd': old['szdd'],
-        'ismoved': 0,  # 如果前一天位置变化这个值会为1，第二天仍然获取到昨天的1，而事实上位置是没变化的，所以置0
-        # 'ismoved': old['ismoved'],
-        'tw': old['tw'],
-        # 'bztcyy': old['bztcyy'], # 2021.8.1 del
-        # 'sftjwh': old['sfsfbh'],  # 2020.9.16 del
-        # 'sftjhb': old['sftjhb'],  # 2020.9.16 del
-        'sfcxtz': old['sfcxtz'],
-        # 'sfyyjc': old['sfyyjc'],# 2021.8.1 del
-        # 'jcjgqr': old['jcjgqr'],# 2021.8.1 del
-        # 'sfjcwhry': old['sfjcwhry'],  # 2020.9.16 del
-        # 'sfjchbry': old['sfjchbry'],  # 2020.9.16 del
-        'sfjcbh': old['sfjcbh'],  # 是否接触病患
-        # 'jcbhlx': old['jcbhlx'], # 2021.1.29 del 接触病患类型
-        'sfcyglq': old['sfcyglq'],  # 是否处于隔离期
-        # 'gllx': old['gllx'],   # 2021.1.29 del 隔离类型
-        'sfcxzysx': old['sfcxzysx'],
-        # 'old_szdd': old['szdd'],# 2021.8.1 del
-        'geo_api_info': old['old_city'],  # 保持昨天的结果
-        'old_city': old['old_city'],
-        'geo_api_infot': old['geo_api_infot'],
-        'date': datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime("%Y-%m-%d"),
-        # 'fjsj': old['fjsj'],  # 返京时间# 2021.8.1 del
-        # 'ljrq': old['ljrq'],  # 离京日期 add@2021.1.24# 2021.8.1 del
-        # 'qwhd': old['qwhd'],  # 去往何地 add@2021.1.24# 2021.8.1 del
-        # 'chdfj': old['chdfj'],  # 从何地返京 add@2021.1.24# 2021.8.1 del
-        # 'jcbhrq': old['jcbhrq'], # del 2021.1.29 接触病患日期
-        # 'glksrq': old['glksrq'], # del 2021.1.29 隔离开始日期
-        # 'fxyy': old['fxyy'],# 2021.8.1 del
-        # 'jcjg': old['jcjg'],# 2021.8.1 del
-        # 'jcjgt': old['jcjgt'],# 2021.8.1 del
-        # 'qksm': old['qksm'],# 2021.8.1 del
-        # 'remark': old['remark'],
-        'jcjgqk': old['jcjgqk'],
-        # 'jcwhryfs': old['jcwhryfs'],# 2021.8.1 del
-        # 'jchbryfs': old['jchbryfs'],# 2021.8.1 del
-        'gtshcyjkzt': old['gtshcyjkzt'],  # add @2020.9.16
-        'jrsfdgzgfxdq': old['jrsfdgzgfxdq'],  # add @2020.9.16
-        'jrsflj': old['jrsflj'],  # add @2020.9.16
-        'app_id': 'ucas'
+
+        "jzdz": "北京市石景山区玉泉路19号甲",     # Residential Address
+        "zrzsdd": "2",                       # Yesterday place to stay    1.雁栖湖  8.京外
+        # Whether you are in school or not  1.是, 主要是在雁栖湖校区   5.否
+        "sfzx": "2",
+        "szgj": "中国",                       # current country
+        "szdd": "国内",                       # current address
+        "dqszdd": "1",                       # current location
+
+        #
+        "address": "北京市怀石景山区",
+        "area": "石景山区",
+        "province": "北京市",
+        "city": "",
+      
+        "geo_api_info": "{\"address\":\"北京市石景山区\",\"details\":\"玉泉路19号甲中国科学院大学玉泉路校区\",\"province\":{\"label\":\"北京市\",\"value\":\"\"},\"city\":{\"label\":\"\",\"value\":\"\"},\"area\":{\"label\":\"石景山区\",\"value\":\"\"}}",
+        "szgj_api_info": "{\"area\":{\"label\":\"\",\"value\":\"\"},\"city\":{\"label\":\"\",\"value\":\"\"},\"address\":\"\",\"details\":\"\",\"province\":{\"label\":\"\",\"value\":\"\"}}",
+        "szgj_select_info": {},
+        #
+
+        # whether you are in high or medium risk area or not  4. 无上述情况
+        "dqsfzzgfxdq": "4",
+        # do you have a travel history in risk area  4. 无上述情况
+        "zgfxljs": "4",
+        "tw": "1",                           # Today’s body temperature 1.37.2℃及以下
+        # Do you have such symptoms as fever, fatigue, dry cough or difficulty in breathing today?
+        "sffrzz": "0",
+        "dqqk1": "1",                        # current situation      1.正常
+        "dqqk1qt": "",
+        "dqqk2": "1",                        # current situation      1.无异常
+        "dqqk2qt": "",
+        # 昨天是否接受核酸检测
+        "sfjshsjc": "1",                     # PCR test?       1.是 0.否
+        # 第一针接种
+        "dyzymjzqk": "3",                    # first vaccination situation  3.已接种
+        "dyzjzsj": "2021-03-27",             # date of first vaccination
+        "dyzwjzyy": "",
+        # 第二针接种
+        "dezymjzqk": "3",                    # second vaccination situation  3.已接种
+        "dezjzsj": "2021-04-21",             # date of second vaccination
+        "dezwjzyy": "",
+        # 第三针接种
+        "dszymjzqk": "3",                    # third vaccination situation  6.未接种
+        "dszjzsj": "2021-11-02",             # default time
+        "dszwjzyy": "",            # reason of non-vaccination
+
+        "gtshryjkzk": "1",                   # health situation
+        "extinfo": "",                       # other information
+        # personal information
+
+        # "created_uid":"0",
+        # "todaysfhsjc":"",
+        # "is_daily":1,
+        "geo_api_infot": "{\"address\":\"北京市石景山区",\"details\":\"玉泉路19号甲中国科学院大学玉泉路校区\",\"province\":{\"label\":\"北京市\",\"value\":\"\"},\"city\":{\"label\":\"\",\"value\":\"\"},\"area\":{\"label\":\"石景山区\",\"value\":\"\"}}",
+
+        # yesterday information
+        "old_szdd": "国内",
+     
     }
 
     check_data_msg = check_submit_data(new_daily)  # 检查上报结果
